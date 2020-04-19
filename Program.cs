2db71748f484be24a075aa2994a537312af7f255
user <user@Williams-iMac.local>
2020-04-19 17:18:25 +0500

    Added Methods Multiply(),Divide(),Sum(),        static T Minus<T>(T x, T y)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HomeWork14
 {
     class Program
     {
+         
+        delegate T Operation<T>(T x, T y);
         static void Main(string[] args)
         {
             
+
+                bool  sss = true;
+                while(sss)
+                {
+                       Console.ForegroundColor = ConsoleColor.Yellow;
+                       System.Console.WriteLine("Choose your operations(*,/,+,-):");
+                       System.Console.WriteLine("1.Multiply(*):");
+                      
+                }   
+            
+
+            
         }
-    }
-}
+        
+        
+        static T Multiply<T>(T x, T y){
+
+            return (dynamic)x * (dynamic)y;
+        }
+        static T Divide<T>(T x, T y){
+
+            return (dynamic)x / (dynamic)y;
+        }
+        static T Sum<T>(T x, T y)
+        {
+            return (dynamic)x + (dynamic)y;
+        }
+
+        static T Minus<T>(T x, T y)
+        {
+            return (dynamic)x - (dynamic)y;
+        }
+     }
+
+    
+
+     
+ }
+
+    
+
+   
